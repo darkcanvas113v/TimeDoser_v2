d3d50dd892876423cf637167a18c67458a97f53d
Darkhan <darkcanvas113v@gmail.com>
2024-06-16 20:25:44 +0900

    Fixed the drag and drop animations

--- composeApp/src/commonMain/kotlin/org/darkcanvas/timedoser/features/main_screen/ui/DefaultFragment.kt
+++ composeApp/src/commonMain/kotlin/org/darkcanvas/timedoser/features/main_screen/ui/DefaultFragment.kt
@@ -1,17 +1,32 @@
 package org.darkcanvas.timedoser.features.main_screen.ui
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListItemInfo
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.zIndex
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import org.darkcanvas.timedoser.data_domain.day_component.domain.model.isInteractive
 import org.darkcanvas.timedoser.features.main_screen.models.TaskUIModel
+import kotlin.math.abs
 
 @Composable
 fun DefaultFragment(
@@ -21,43 +36,65 @@ fun DefaultFragment(
   onItemDisabled: (Int) -> Unit,
   tasks: List<TaskUIModel>
 ) {
-  var offsets by remember {
-    mutableStateOf<List<Int>>(listOf())
+
+  val state = rememberLazyListState()
+  val scope = rememberCoroutineScope()
+  var firstInteractable by remember { mutableStateOf(0) }
+
+  LaunchedEffect(tasks) {
+    firstInteractable = tasks.indexOfFirst { it.state.isInteractive() }
   }
 
-  LaunchedEffect(tasks.size) {
-    offsets = List(tasks.size) { 0 }
+  val draggableState = remember() {
+    DragState(
+      lazyListState = state,
+      scope = scope,
+      onItemMoved = onItemMoved
+    )
   }
 
-  if (offsets.size == tasks.size)
   LazyColumn(
-    modifier = Modifier.fillMaxHeight()
+    state = state,
+    modifier = Modifier
+      .fillMaxHeight()
+      .pointerInput(draggableState.dragCancelMagicNumber) {
+        detectDragGesturesAfterLongPress(
+          onDrag = { change, offset ->
+            change.consume()
+            draggableState.onDrag(offset, firstInteractable)
+          },
+          onDragStart = { offset ->
+
+            val index = draggableState.getIndexForOffset(offset)
+            if (index != null && index >= firstInteractable) {
+              draggableState.onDragStart(index)
+            } else {
+              draggableState.cancelDrag()
+            }
+          },
+          onDragEnd = draggableState::finishDrag,
+          onDragCancel = {
+            if (draggableState.currentOffset.x < -600)
+              onItemDisabled(draggableState.currentIndex)
+            else if (draggableState.currentOffset.x > 600)
+              onItemRemoved(draggableState.currentIndex)
+            draggableState.finishDrag()
+          }
+        )
+      }
   ) {
     itemsIndexed(
       items = tasks
     ) { i, item ->
-      DraggableItem(
-        isDraggable = item.state.isInteractive(),
-        offsetPos = offsets[i],
-        offsetChanged = { offset ->
-          val bottom = tasks.indexOfFirst { it.state.isInteractive() }
-          val cappedOffset = capOffset(offset, i, tasks.size, bottom)
-          offsets = calculateOffsets(cappedOffset, tasks.size, i)
-        },
-        onDragCancelled = {
-          offsets = List(tasks.size) { 0 }
-        },
-        onDragFinished = {
-          val offset = offsets[i]
-          offsets = List(tasks.size) { 0 }
-          if (offset != 0) onItemMoved(i, i + offset)
-        },
-        onSwipedToRight = {
-          onItemRemoved(i)
-        },
-        onSwipedToLeft = {
-          onItemDisabled(i)
-        }
+      val itemState by remember { derivedStateOf {draggableState.resolveDraggableState(i)} }
+
+      Box(
+        modifier = Modifier.graphicsLayer {
+          translationY = itemState.y
+          translationX = itemState.x
+        }.zIndex(
+          itemState.z
+        )
       ) {
         TaskItem(task = item, onClick = { onItemClick(i) })
       }
@@ -65,14 +102,110 @@ fun DefaultFragment(
   }
 }
 
+class DragState(
+  val lazyListState: LazyListState,
+  val scope: CoroutineScope,
+  val onItemMoved: (Int, Int) -> Unit
+) {
+  var itemInfo by mutableStateOf<LazyListItemInfo?>(null)
+  var currentIndex by mutableStateOf(0)
+  var prevIndex by mutableStateOf(-1)
+  var currentOffset by mutableStateOf(DragOffset(0f, 0f))
+  val prevOffset = Animatable(DragOffset.DEFAULT, DragOffset.converter)
+  var dragCancelMagicNumber by mutableStateOf(0)
+
+  fun finishDrag() {
+    prevIndex = currentIndex
+    currentIndex = -1
+    scope.launch {
+      prevOffset.snapTo(currentOffset)
+      currentOffset = DragOffset.DEFAULT
+      prevOffset.animateTo(DragOffset.DEFAULT)
+    }
+  }
+
+  fun cancelDrag() {
+    dragCancelMagicNumber++
+  }
+
+  fun resolveDraggableState(index: Int) = when (index) {
+    currentIndex -> {
+      DraggableItemState(
+        y = currentOffset.y,
+        x = currentOffset.x,
+        z = 3f
+      )
+    }
+
+    prevIndex -> {
+      DraggableItemState(
+        y = prevOffset.value.y,
+        x = prevOffset.value.x,
+        z = 2f
+      )
+    }
+
+    else -> DraggableItemState()
+  }
+
+  fun getIndexForOffset(offset: Offset): Int? {
+    return lazyListState.layoutInfo.visibleItemsInfo.firstOrNull { item -> offset.y.toInt() in item.offset..item.offset + item.size }
+      ?.index
+  }
+
+  fun onDragStart(index: Int) {
+    lazyListState.layoutInfo.visibleItemsInfo[index].let {
+      currentIndex = index
+      itemInfo = it
+    }
+  }
+
+  fun onDrag(offset: Offset, firstInteractable: Int) {
+    currentOffset = currentOffset.translate(offset)
+
+    itemInfo?.let { info ->
+      val itemOffset = capOffset(
+        (currentOffset.y / info.size).toInt(),
+        currentIndex,
+        lazyListState.layoutInfo.totalItemsCount,
+        firstInteractable
+      )
+      if (abs(itemOffset) > 0) {
+        onItemMoved(currentIndex, currentIndex + itemOffset)
+
+        scope.launch {
+          prevOffset.snapTo(DragOffset(currentOffset.x, itemOffset.toFloat() * info.size))
+          prevIndex = currentIndex
+
+          currentIndex += itemOffset
+          currentOffset =
+            currentOffset.translate(Offset(0f, -itemOffset * info.size.toFloat()))
+
+          prevOffset.animateTo(DragOffset.DEFAULT)
+
+        }
+      }
+    }
+
+    if (abs(currentOffset.x) > 600) {
+      dragCancelMagicNumber++
+    }
+  }
+}
+
+data class DraggableItemState(
+  val x: Float = 0f,
+  val y: Float = 0f,
+  val z: Float = 1f,
+)
+
 fun calculateOffsets(offset: Int, size: Int, pos: Int): List<Int> {
   val list = MutableList(size) { 0 }
   list[pos] = offset
 
   if (offset > 0) {
     for (i in pos + 1 until pos + offset + 1) list[i] = -1
-  }
-  else {
+  } else {
     for (i in pos + offset until pos) list[i] = 1
   }
 
--- composeApp/src/commonMain/kotlin/org/darkcanvas/timedoser/features/main_screen/ui/DraggableItem.kt
+++ composeApp/src/commonMain/kotlin/org/darkcanvas/timedoser/features/main_screen/ui/DraggableItem.kt
@@ -1,34 +1,12 @@
 package org.darkcanvas.timedoser.features.main_screen.ui
 
-import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.AnimationVector2D
 import androidx.compose.animation.core.TwoWayConverter
-import androidx.compose.animation.core.VectorConverter
-import androidx.compose.animation.core.animateOffsetAsState
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
-import androidx.compose.foundation.gestures.rememberDraggableState
-import androidx.compose.foundation.layout.Box
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.changedToDown
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.zIndex
-import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.launch
 import kotlin.math.abs
 
 
-private data class DragOffset(
+data class DragOffset(
   val x: Float = 0f,
   val y: Float = 0f,
   val mode: Mode = when {
@@ -43,24 +21,22 @@ private data class DragOffset(
     val newX = x + offset.x
     val newYAbs = abs(newY)
     val newXAbs = abs(newX)
-    var newMode = mode
 
-    if (mode == Mode.Vertical && newYAbs < yThreshold) newMode = Mode.Undefined
-    else if (mode == Mode.Horizontal && newXAbs < xThreshold) newMode = Mode.Undefined
-
-    return when (newMode) {
+    return when (mode) {
       Mode.Vertical -> {
         copy(
           y = newY,
           x = 0f
         )
       }
+
       Mode.Horizontal -> {
         copy(
           x = newX,
           y = 0f
         )
       }
+
       Mode.Undefined -> {
         if (newXAbs > xThreshold) copy(
           x = newX,
@@ -96,78 +72,4 @@ private data class DragOffset(
       convertFromVector = { DragOffset(it.v1, it.v2) }
     )
   }
-}
-
-
-@Composable
-fun DraggableItem(
-  isDraggable: Boolean,
-  offsetChanged: (Int) -> Unit,
-  onDragCancelled: () -> Unit,
-  onDragFinished: (Int) -> Unit,
-  onSwipedToLeft: () -> Unit,
-  onSwipedToRight: () -> Unit,
-  offsetPos: Int,
-  content: @Composable () -> Unit,
-) {
-  var zIndex by remember { mutableStateOf(0f) }
-  var size by remember() { mutableStateOf(IntSize.Zero) }
-  val offsetAnim = remember { Animatable(DragOffset.DEFAULT, DragOffset.converter) }
-  Offset.VectorConverter
-  var dragCancelMagicNumber by remember { mutableStateOf(0) }
-
-  val scope = rememberCoroutineScope()
-
-
-  LaunchedEffect(offsetAnim.value.y) {
-    if (offsetAnim.isRunning) return@LaunchedEffect
-
-    val currentOffset = (offsetAnim.value.y / size.height).toInt()
-    if (offsetPos != currentOffset)
-      offsetChanged(currentOffset)
-  }
-
-  if (isDraggable)
-    Box(
-      modifier = Modifier
-        .onGloballyPositioned { size = it.size }
-        .graphicsLayer {
-          translationX = offsetAnim.value.x
-          translationY = if (offsetAnim.value.y != 0f) offsetAnim.value.y else size.height * offsetPos.toFloat()
-        }
-        .zIndex(zIndex)
-        .pointerInput(dragCancelMagicNumber) {
-            detectDragGesturesAfterLongPress(
-              onDrag = { change, dragAmount ->
-                change.consume()
-                scope.launch { offsetAnim.snapTo(offsetAnim.value.translate(dragAmount)) }
-
-                if (abs(offsetAnim.value.x) > 600) {
-                  dragCancelMagicNumber++
-                }
-              },
-              onDragStart = {
-                zIndex = 2f
-              },
-              onDragEnd = {
-                scope.launch { offsetAnim.animateTo(DragOffset.DEFAULT) }
-                zIndex = 1f
-                onDragFinished(offsetPos)
-              },
-              onDragCancel = {
-                if (offsetAnim.value.x < -600)
-                  onSwipedToLeft()
-                else if (offsetAnim.value.x > 600)
-                  onSwipedToRight()
-
-                scope.launch { offsetAnim.animateTo(DragOffset.DEFAULT) }
-                zIndex = 1f
-                onDragCancelled()
-              }
-            )
-        }
-    ) {
-      content()
-    }
-  else content()
 }
